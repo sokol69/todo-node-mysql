7ab05a92e2cdef5255455f4791d57b313dc296d2
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-07-14 08:50:01 +0300

    Added graphql

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const path = require("path");
+const { graphqlHTTP } = require("express-graphql");
 
+const schema = require("./graphql/schema");
+const resolver = require("./graphql/resolver");
 const sequelize = require("./utils/database");
-const todoRoutes = require("./routes/todo");
+const { Resolver } = require("dns");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use("/api/todo", todoRoutes);
+
+app.use(
+  graphqlHTTP({
+    schema,
+    rootValue: resolver,
+  })
+);
 
 app.use((req, res, next) => {
   res.sendFile("/index.html");
